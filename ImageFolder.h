cb45b812695bc1d2f5a5f186871263879d57af4c
Vadikvs666 <vad.86@bk.ru>
2014-11-29 16:37:46 +0400

    Create ImageFolder.h

---
+++ ImageFolder.h
@@ -0,0 +1,51 @@
+#ifndef IMAGEFOLDER_H
+#define IMAGEFOLDER_H
+
+#include <QMainWindow>
+#include <QVector>
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+
+#include "QFileDialog"
+#include "QFile"
+#include "QTextStream"
+
+#include <QStatusBar>
+
+class ImageFolder : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ImageFolder(QWidget *parent = 0);
+    ~ImageFolder();
+
+    QAction *openAction  ;
+    QAction *syncAction ;
+    QAction *exitAction;
+    QAction *loadSyteAction;
+    QAction *paramAction;
+
+    QMenuBar *menu;
+    QStatusBar *bar;
+
+    
+
+  
+
+
+signals:
+    void goodbuy();
+    void compleet();
+
+private slots:
+    void quit();//выход из приложения
+    void open();//функция открытия файла прайса
+ 
+
+
+
+};
+
+#endif // IMAGEFOLDER_H
